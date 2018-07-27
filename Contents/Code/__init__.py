e3f0e6549db47a065917ea74cc5562d46b9bf3dc
Leo Liu <wasabinz@gmail.com>
2018-07-27 18:11:56 +1200

    formatting changes

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -172,7 +172,7 @@ class DMMAgent(Agent.Movies):
 
     def update(self, metadata, media, lang):
 
-        self.log('***** UPDATING "%s" ID: %s - DMMAgent v.%s *****',
+        self.log('***** UPDATING "{}" ID: {} - DMMAgent v.{} *****',
                  media.title, metadata.id, VERSION_NO)
         try:
             # Make url
